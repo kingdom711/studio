ad1154e39df91e8fad56c0f446b0e69534f0b001
김종욱찾기 <wkjow1@gmail.com>
2025-11-28 06:17:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6524876617-36660",
+  "appId": "1:1007850836016:web:28a71d9f2c23f816a81df5",
+  "apiKey": "AIzaSyB7QBIimHIHFs49YyI7mu8YrUvMqLdtwYY",
+  "authDomain": "studio-6524876617-36660.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1007850836016"
+};
